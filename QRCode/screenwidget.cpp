267afcb82825fba6f75a7da1c6ad4fdce9d5aaac
Yeun <609627425@qq.com>
2015-10-24 21:32:47 +0800

    V0.3

--- QRCode/screenwidget.cpp
+++ QRCode/screenwidget.cpp
@@ -4,155 +4,248 @@
 #include <QApplication>
 #include <QDesktopWidget>
 #include <QMessageBox>
+#include <QFileDialog>
 #include <QClipboard>
 #include <QDebug>
 
 ScreenWidget::ScreenWidget(QWidget *parent) :
     QWidget(parent)
-{
-    Init();  
-    m_isDrawing = false;
-    m_selected = false;
-    m_beginPos = QPoint(0,0);
-    m_endPos = QPoint(QApplication::desktop()->width(),QApplication::desktop()->height());
+{ 
+    InitWdiget();
+    InitConnect();
+    m_rubberband->installEventFilter(this);
     setWindowFlags(Qt::WindowStaysOnTopHint);
 }
-
-
-void ScreenWidget::Init() //初始化菜单
+void ScreenWidget::InitWdiget()     //初始化变量
 {
+    m_isDrawing = false;
+    m_selected = false;
+    leftMousePress = false;
+    m_rubberband = new QRubberBand(QRubberBand::Rectangle);
+
+    /*————————————————————————————————————————————————————————————————————————————————————-*/
     a_begin = new QAction(tr("截屏"),this);
-    a_begin->setShortcut(tr("Ctrl+S"));
-    a_save = new QAction(tr("完成"),this);
+    a_get = new QAction(tr("完成"),this);
+    a_save = new QAction(tr("保存"),this);
     a_cancel = new QAction(tr("取消"),this);
     a_search = new QAction(tr("扫码"),this);
     a_exit = new QAction(tr("退出"),this);
-    a_exit->setShortcut(Qt::CTRL + Qt::Key_X);  //未解决问题
-
-    connect(a_begin,SIGNAL(triggered()),this,SLOT(BeginScreenShot()));
-    connect(a_save,SIGNAL(triggered()),this,SLOT(GetScreenShot()));
-    connect(a_cancel,SIGNAL(triggered()),this,SLOT(CancelScreenShot()));
-    connect(a_search,SIGNAL(triggered()),this,SLOT(SearchCode()));
-    connect(a_exit,SIGNAL(triggered()),this,SLOT(close()));
 
     trayicon = new QSystemTrayIcon(this);
-    trayicon->setIcon(QIcon(":/Icon/trayicon"));
     traymenu = new QMenu(this);
+    /*————————————————————————————————————————————————————————————————————————————————————-*/
+    trayicon->setIcon(QIcon(":/Icon/trayicon"));
     trayicon->setContextMenu(traymenu);
     traymenu->addAction(a_begin);
     traymenu->addSeparator();
-    traymenu->addAction(a_exit);   
-
+    traymenu->addAction(a_exit);
     trayicon->show();       //显示托盘
     trayicon->setToolTip(tr("截图工具"));
     //trayicon->showMessage(tr("Yoooo~~~"), tr("莫慌，有我"), QSystemTrayIcon::Information, 5000);
+    m_rubberband->addAction(a_get);
+    m_rubberband->addAction(a_save);
+    m_rubberband->addAction(a_cancel);
+    m_rubberband->addAction(a_search);
+    m_rubberband->setContextMenuPolicy(Qt::ActionsContextMenu);
+}
+
+void ScreenWidget::InitConnect() //初始化槽
+{
+    connect(a_begin,SIGNAL(triggered()),this,SLOT(BeginScreenShot()));
+    connect(a_get,SIGNAL(triggered()),this,SLOT(GetScreenShot()));
+    connect(a_save,SIGNAL(triggered()),this,SLOT(SaveScreenShot()));
+    connect(a_cancel,SIGNAL(triggered()),this,SLOT(CancelScreenShot()));
+    connect(a_search,SIGNAL(triggered()),this,SLOT(SearchCode()));
+    connect(a_exit,SIGNAL(triggered()),this,SLOT(close()));   
+}
+
+bool ScreenWidget::eventFilter(QObject *o, QEvent *e)       //rubberband点击事件
+{
+    if (o != m_rubberband)
+    {
+        return QWidget::eventFilter(o, e);
+    }
+
+    QMouseEvent *mouseEvent = static_cast<QMouseEvent*> (e);
+    //true 鼠标左键按下且按键还未弹起
+    leftMousePress = true;
+    if ((mouseEvent->button() == Qt::LeftButton)
+        && (mouseEvent->type() == QEvent::MouseButtonPress))
+    {
+        m_beginPos = mouseEvent->pos();
+        return true;
+    }
+    //true 鼠标左键按下并拖动
+    if ((mouseEvent->type() == QEvent::MouseMove)
+        && (leftMousePress))
+    {
+        m_endPos = mouseEvent->pos();
+        int x = m_rubberband->x(),y = m_rubberband->y();
+        int w = m_rubberband->width(),h = m_rubberband->height();
+        int offx = m_endPos.x() - m_beginPos.x();
+        int offy = m_endPos.y() - m_beginPos.y();
+        if(x+offx>=0 && x+offx+w <= QApplication::desktop()->width())
+            x += offx;
+        if(y + offy >= 0 && y+offy+h <= QApplication::desktop()->height())
+            y += offy;
+        m_rubberband->move(x,y);
+        return true;
+    }
+
+    //鼠标左键松开
+    if ((mouseEvent->button() == Qt::LeftButton)
+        && (mouseEvent->type() == QEvent::MouseButtonRelease))
+    {
+        leftMousePress = false;
+        return true;
+    }
+
+    return false;
 }
 
 void ScreenWidget::closeEvent(QCloseEvent *event)       //关闭事件
 {
     hide();
     delete a_begin;
+    delete a_get;
     delete a_save;
     delete a_cancel;
     delete a_search;
     delete a_exit;
     delete trayicon;
     delete traymenu;
+    delete m_rubberband;
     event->accept();
 }
-
 
 void ScreenWidget::showEvent(QShowEvent *event)
 {
     showFullScreen();
     setCursor(Qt::CrossCursor);
+    QWidget::showEvent(event);
 }
 
 void ScreenWidget::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
-    painter.drawPixmap(0, 0, m_tempscreen);
-    QPen pen(QColor(0,0,255));
-    pen.setWidth(2);
-    painter.setPen(pen);
-    painter.drawRect(QRectF(m_beginPos,m_endPos));
-
-    painter.setPen(QColor(Qt::black));
-    m_screenwidth = abs(m_endPos.x() - m_beginPos.x());
-    m_screenheight = abs(m_endPos.y() - m_beginPos.y());
-    painter.drawText(0,10,QString("%1x%2").arg(m_screenwidth).arg(m_screenheight));
+    painter.drawPixmap(0, 0, m_screen);
+    if(m_selected == false)
+    {
+        QPen pen(QColor(0,255,255));
+        pen.setWidth(2);
+        painter.setPen(pen);
+        painter.drawRect(QApplication::desktop()->rect());
+    }
+    QWidget::paintEvent(event);
 }
 
 void ScreenWidget::mousePressEvent(QMouseEvent *event)  //鼠标按下事件
 {
+
+    m_beginPos = event->pos();
     if (event->button() == Qt::LeftButton) {
+        if(m_rubberband->isVisible())
+        {
+            m_rubberband->hide();
+            m_rubberband->show();
+            return;
+        }
         m_isDrawing = true;
-        m_beginPos = event->pos();
+        m_selected = true;
+        update();
+        m_rubberband->setGeometry(QRect(m_beginPos,QSize()));
+        m_rubberband->show();
     }
 }
 
 void ScreenWidget::mouseMoveEvent(QMouseEvent *event)   //鼠标移动事件
 {
     if (m_isDrawing) {
-        m_tempscreen = m_screen;
         m_endPos = event->pos();
-        update();
+        m_rubberband->setGeometry(QRect(m_beginPos, m_endPos).normalized());
     }
 }
 
 void ScreenWidget::mouseReleaseEvent(QMouseEvent *event)    //鼠标释放事件
 {
-    if (event->button() == Qt::LeftButton) {
-        m_endPos = event->pos();
-        if(m_beginPos == m_endPos)
-        {
-            QPixmap pix = m_screen.grabWidget(this,m_selectedsize.x(),m_selectedsize.y(),m_selectedsize.width(),m_selectedsize.height());
-            QApplication::clipboard()->setImage(pix.toImage());
-            m_selected = false;
-            m_beginPos = QPoint(0,0);
-            m_endPos = QPoint(QApplication::desktop()->width(),QApplication::desktop()->height());
-            hide();
-        }
+    m_endPos = event->pos();
+    if (event->button() == Qt::LeftButton
+            && (!m_rubberband->isVisible())) {
         m_isDrawing = false;
-        m_selected = true;
-        m_selectedsize = QRect(m_beginPos,m_endPos);
-        //savePixMap();
     }
     else if(event->button() == Qt::RightButton){
-        if(m_selected)
-        {
-            m_selected = false;
-            m_tempscreen = m_screen;
-            m_beginPos = QPoint(0,0);
-            m_endPos = QPoint(QApplication::desktop()->width(),QApplication::desktop()->height());
-            update();
-        }
-        else
-            hide();
+        CancelScreenShot();
     }
 }
 
+void ScreenWidget::mouseDoubleClickEvent(QMouseEvent *event)    //双击事件
+{
+    if(m_rubberband->size() == QSize(0,0)){
+        m_rubberband->hide();
+        m_rubberband->setGeometry(QApplication::desktop()->rect().normalized());
+        GetScreenShot();
+    }
+    else
+        GetScreenShot();
+    QWidget::mouseDoubleClickEvent(event);
+}
 
 void ScreenWidget::BeginScreenShot()    //开始截图
 {
     m_screen = QPixmap::grabWindow(QApplication::desktop()->winId());
-    m_tempscreen = m_screen;
     show();
 }
 
 
-void ScreenWidget::GetScreenShot()  //完成截图
+void ScreenWidget::SaveScreenShot()  //保存截图
 {
-    qDebug() << "完成截图" ;
+    QString fileName = QFileDialog::getSaveFileName(this,
+                                                tr("Open Image"),
+                                                ".",
+                                                tr("Image Files(*.JPG *.PNG)"));
+    if (!fileName.isNull())
+    {
+        QPixmap pix = m_screen.grabWidget(this,m_rubberband->x(),m_rubberband->y(),m_rubberband->width(),m_rubberband->height());
+        pix.save(fileName);
+        m_rubberband->hide();
+        hide();
+        m_screen = QPixmap();
+    }
+    else{
+        m_rubberband->hide();
+        m_rubberband->show();
+    }
+}
+
+void ScreenWidget::GetScreenShot()      //完成截图
+{
+    QPixmap pix = m_screen.grabWidget(this,m_rubberband->x(),m_rubberband->y(),m_rubberband->width(),m_rubberband->height());
+    QApplication::clipboard()->setImage(pix.toImage());
+    m_selected = false;
+    m_rubberband->hide();
+    hide();
+    m_screen = QPixmap();
 }
 
 void ScreenWidget::CancelScreenShot() //取消截图
 {
-    qDebug() << "取消截图" ;
+    if(m_selected){
+        m_selected = false;
+        m_rubberband->hide();
+        update();
+    }
+    else{
+        m_selected = false;
+        m_rubberband->hide();
+        hide();
+        m_screen = QPixmap();
+    }
 }
 
 void ScreenWidget::SearchCode() //扫描二维码
 {
     qDebug() << "扫描二维码" ;
 }
+
+
 
--- QRCode/screenwidget.h
+++ QRCode/screenwidget.h
@@ -7,6 +7,7 @@
 #include <QLabel>
 #include <QMenu>
 #include <QSystemTrayIcon>
+#include <QRubberBand>
 #include <QMouseEvent>
 
 class ScreenWidget : public QWidget
@@ -17,6 +18,7 @@ public:
 
 private:
     QAction* a_begin;           //截屏
+    QAction* a_get;             //完成
     QAction* a_save;            //保存
     QAction* a_cancel;          //取消
     QAction* a_search;          //扫码
@@ -24,34 +26,35 @@ private:
 
     QSystemTrayIcon* trayicon;  //托盘
     QMenu* traymenu;            //托盘菜单
+    QRubberBand* m_rubberband;  //橡皮框
 
     QPoint m_beginPos;           //起始点
     QPoint m_endPos;             //终点
     QPixmap m_screen;            //截图
-    QPixmap m_tempscreen;        //缓冲区
 
-    int m_screenwidth;          //整个屏幕的宽度
-    int m_screenheight;         //整个屏幕的高度
-    QRect m_selectedsize;       //选区范围
     bool m_isDrawing;           //是否在画
     bool m_selected;            //是否选中
-
+    bool leftMousePress;        //左键按下
 
 public slots:
     void BeginScreenShot();     //开始截图
+    void SaveScreenShot();      //保存截图
     void GetScreenShot();       //完成截图
     void CancelScreenShot();    //取消截图
     void SearchCode();          //扫描二维码
-public:
-    void Init();                //初始化菜单
+private:
+    void InitWdiget();          //初始化变量
+    void InitConnect();         //初始化槽
 
-public:
+protected:
+    virtual bool eventFilter(QObject *o, QEvent *e);    //触发事件
     void closeEvent(QCloseEvent* event);                //关闭事件
     void showEvent(QShowEvent* event);                  //显示事件
     void paintEvent(QPaintEvent* event);                //绘图事件
     void mousePressEvent(QMouseEvent *event);           //鼠标按下事件
     void mouseMoveEvent(QMouseEvent *event);            //鼠标移动事件
     void mouseReleaseEvent(QMouseEvent *event);         //鼠标释放事件
+    void mouseDoubleClickEvent(QMouseEvent *event);     //双击事件
 };
 
 #endif // SCREENWIDGET_H
